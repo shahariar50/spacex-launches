af7ba4addc9add64cf01f189d87a050910514e00
Shahariar Touhid <shahariar.touhid@gmail.com>
2021-09-24 02:05:00 +0600

    added search bar and make it functional

--- src/App.js
+++ src/App.js
@@ -7,25 +7,58 @@ import { loadLaunches } from "./store/reducer/launchesReducer";
 const mapStateToProps = ({ launches }) => ({ launches });
 
 function App({ launches, dispatch }) {
+  const [searchValue, setSearchValue] = React.useState("");
+  const [filterValue, setFilterValue] = React.useState("");
+
   React.useEffect(() => {
     dispatch({ type: loadLaunches });
   }, [dispatch]);
+
+  const handleSearch = ({ target }) => {
+    setSearchValue(target.value);
+
+    setFilterValue("");
+  };
+
+  const filterData = () => {
+    const filterSearch = launches?.data?.filter((item) =>
+      item.rocket.rocket_name
+        .replace(/ /g, "")
+        .toLowerCase()
+        .includes(searchValue.replace(/ /g, "").toLowerCase())
+    );
+
+    return filterSearch;
+  };
 
   return (
     <div className="App">
       <Header />
       <div className="pt-4 pb-4">
         <div className="container">
-          <div className="row jusitfy-content-between">
-            <div className="col-sm-4"></div>
-            <div className="col-sm-6"></div>
+          <div className="row jusitfy-content-between mb-4">
+            <div className="col-sm-4">
+              <input
+                type="text"
+                value={searchValue}
+                className="form-control form-control-lg"
+                placeholder="search launches by rocket name...."
+                onChange={handleSearch}
+              />
+            </div>
+            <div className="col"></div>
           </div>
           <div className=" row align-items-stretch">
-            {launches?.data?.map((item) => (
-              <div class="col-md-4 mb-4" key={item.flight_number}>
-                <LaunchItemCard data={item} />
-              </div>
-            ))}
+            {filterData().length > 0
+              ? filterData().map((item) => (
+                  <div
+                    class="col-md-4 mb-4"
+                    key={`${item.launch_date_unix}${item.flight_number}`}
+                  >
+                    <LaunchItemCard data={item} />
+                  </div>
+                ))
+              : "Not enough lunches to show..."}
           </div>
         </div>
       </div>
--- src/service/launcheService.js
+++ src/service/launcheService.js
@@ -7,9 +7,17 @@ export async function getLaunchesApi() {
     .catch((err) => err);
 }
 
-export async function getLaunchesByFilterApi() {
+export async function getLaunchesByFilterApi(filters) {
+  let endpoint = `/orders?order_status=${filters.status}&search=${
+    filters.search ? filters.search : ""
+  }`;
+
   return axios
-    .get("https://api.spacexdata.com/v3/launches")
-    .then(({ data }) => data)
-    .catch((err) => err);
+    .get(`https://api.spacexdata.com/v3/launches/?`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch(() => {
+      return false;
+    });
 }
